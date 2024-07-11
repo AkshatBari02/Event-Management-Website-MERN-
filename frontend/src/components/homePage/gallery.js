f4c2996b185e188ff6e97d2c93bda8b10cc9320c
Akshatbari02 <akshatbari.ab@gmail.com>
2024-07-11 19:55:38 +0530

    commit

--- frontend/src/components/homePage/gallery.js
+++ frontend/src/components/homePage/gallery.js
@@ -16,13 +16,13 @@ function Gallery(){
                         </div>
                         <div className="col-md-4 col-lg-4 col-sm-6 col-12">
                             <Link to="/gallery#Corporate">
-                                <img src={require(`./../../images/wedding-3.jpg`)} alt="wedding" className="gallery-container-img img-2"/>
+                                <img src={require(`./../../images/corporate-6.jpg`)} alt="wedding" className="gallery-container-img img-2"/>
                             </Link>
                             <h4>Corporate Events</h4>
                         </div>
                         <div className="col-md-4 col-lg-4 col-sm-6 col-12">
                             <Link to="/gallery#Social">
-                                <img src={require(`./../../images/wedding-3.jpg`)} alt="wedding" className="gallery-container-img img-3"/>
+                                <img src={require(`./../../images/social-4.jpg`)} alt="wedding" className="gallery-container-img img-3"/>
                             </Link>
                             <h4>Social Events</h4>
                         </div>
